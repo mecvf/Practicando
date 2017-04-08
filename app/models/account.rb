f30412610cbddf6bb6ce138750f5300d894d4e30
mecvf <mauge58@gmail.com>
2017-04-08 19:45:46 -0300

    Ultima actualizacion

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,10 @@
 class Account < ApplicationRecord
+ 
+
+  validates :name, presence: true
+  validates :kind, inclusion: { in: %w(bank cash credit-card) }
+  validates :balance, numericality: true
+  validates :currency, inclusion: { in: %w(USD UYU ARS) }
+
+  belongs_to :user
 end
